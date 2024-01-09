a90978264db2f63a719f1a398c2940c2e2efc3be
Ratna Amalia Azzahra <amaliaratna41@gmail.com>
2024-01-09 23:14:31 +0700

    Event Handler for login and logout

---
+++ src/components/Elements/Input/Label.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable react/prop-types */
+const Label = (props) => {
+    const {htmlFor, children} = props; 
+    return (
+        <label 
+        htmlFor={htmlFor}
+        className="block text-slate-700 font-bold mb-2">
+            {children}
+        </label>
+    )
+}
+
+export default Label;
---
+++ src/pages/products.jsx
@@ -0,0 +1,61 @@
+/* eslint-disable react/jsx-key */
+import { Fragment } from "react"
+import CardProduct from "../components/Fragments/CardProduct"
+import Button from "../components/Elements/Button";
+
+const products = [
+    {
+        id: 1,
+        name: "Sepatu Baru",
+        price: "Rp 1.000.000",
+        image: "/images/shoes-1.jpg",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam animi illum aliquid eos corrupti consequuntur accusamus ipsum quis architecto vero, sequi perspiciatis. Iure quo, in quos consectetur eum distinctio earum?"
+    },
+    {
+        id: 2,
+        name: "Sepatu Lama",
+        price: "Rp 500.000",
+        image: "/images/shoes-1.jpg",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam animi illum aliquid eos"
+    },
+    {
+        id: 3,
+        name: "Sepatu Adadong",
+        price: "Rp 2.000.000",
+        image: "/images/shoes-1.jpg",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam animi illum aliquid eos"
+    }
+]
+
+const email = localStorage.getItem('email');
+
+const ProductsPage = () => {
+
+    const handleLogout = () => {
+        localStorage.removeItem('email');
+        localStorage.removeItem('password');
+        window.location.href = "/login"
+    }
+
+    return (
+        <Fragment>
+            <div className="flex justify-end h-20 bg-blue-600 text-white items-center px-10">
+                {email}
+                <Button className="ml-5 bg-black" onClick={handleLogout}>Logout</Button>
+            </div>
+            <div className="flex justify-center py-5">
+            {products.map((product) => (
+                <CardProduct key={product.id}>
+                    <CardProduct.Header image={product.image}/>
+                    <CardProduct.Body name={product.name}>
+                        {product.description}
+                    </CardProduct.Body>
+                    <CardProduct.Footer price={product.price}/>
+                </CardProduct>
+            ))}
+        </div>
+        </Fragment>
+    )
+}
+
+export default ProductsPage
