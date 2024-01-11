5614859eee9292daf359d7ebd805ae3190cebe4a
Ratna Amalia Azzahra <amaliaratna41@gmail.com>
2024-01-11 22:01:15 +0700

    auto input on email form using useRef

--- src/components/Elements/Input/index.jsx
+++ src/components/Elements/Input/index.jsx
@@ -1,15 +1,17 @@
+/* eslint-disable react/display-name */
 /* eslint-disable react/prop-types */
 import Label from "./Label";
 import Input from "./Input";
+import { forwardRef } from "react";
 
-const InputForm = (props) => {
+const InputForm = forwardRef((props, ref) => {
     const {label, name, type, placeholder} = props;
     return (
         <div className="mb-6">
             <Label htmlFor={name}>{label}</Label>
-            <Input name={name} type={type} placeholder={placeholder}/>
+            <Input name={name} type={type} placeholder={placeholder} ref={ref}/>
         </div>
     )
-}
+})
 
 export default InputForm;
--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -2,7 +2,7 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/jsx-key */
-import { Fragment, useEffect, useState } from "react"
+import { Fragment, useEffect, useRef, useState } from "react"
 import CardProduct from "../components/Fragments/CardProduct"
 import Button from "../components/Elements/Button";
 // import Counter from "../components/Fragments/Counter";
@@ -68,6 +68,27 @@ const ProductsPage = () => {
         }
     };
 
+    // useRef
+    const cartRef = useRef(
+        JSON.parse(localStorage.getItem("cart")) || []
+    );
+
+    const handleAddToCartRef = (id) => {
+        cartRef.current = [...cartRef.curren, { id, qty: 1 }];
+        localStorage.setItem("cart", JSON.stringify(cartRef.current))
+    }
+
+    const totalPriceRef = useRef(null);
+    // console.log(totalPriceRef)
+
+    useEffect(() => {
+        if (cart.length > 0) {
+            totalPriceRef.current.style.display = "table-row"
+        } else {
+            totalPriceRef.current.style.display = "none"
+        }
+    })
+
     return (
         <Fragment>
             <div className="flex justify-end h-20 bg-blue-600 text-white items-center px-10">
@@ -114,7 +135,7 @@ const ProductsPage = () => {
                                 </tr>
                             )
                         })}
-                        <tr>
+                        <tr ref={totalPriceRef}>
                             <td colSpan={3}><b>Price</b></td>
                             <td>
                                 <b>
